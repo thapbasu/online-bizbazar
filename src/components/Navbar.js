73d733b7e70b863968feebacf892d04192ef04af
Basu Thapa <thapabasu000@gmail.com>
2023-04-25 16:44:17 +0545

    all images updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,20 @@ import '../styles/navbar.css';
 import {NavLink,Link} from 'react-router-dom'
 import { Search } from '@material-ui/icons';
 import imageSlider from '../img/slider.PNG';
+import imageHamburger from '../img/Hamburger.png';
+import imageLogo from '../img/logo.png';
+import imageCall from '../img/call.png';
+import barchart from '../img/barchart.png';
+import heart from '../img/heart.png';
+import imageUser from '../img/user.png';
+import shoppingcart from '../img/shoppingcart.png';
+import imageCamera from '../img/camera.png';
+import imageDress from '../img/dress.png';
+import downarrow from '../img/downarrow.png';
+import imageSofa from '../img/sofa.png';
+import essentials from '../img/essentials.png';
+import imageFootball from '../img/football.png';
+
 
 const Navbar = ({size, setShow}) => {
   return (
@@ -24,12 +38,12 @@ const Navbar = ({size, setShow}) => {
       <div className="row align-items-center">
         <div className="col-1-auto">
         <Link className=' icon'>
-                  <img src ="images/hamburger.png" alt="" /></Link>
+                  <img src ={imageHamburger} alt="" /></Link>
         </div>
       
         <div className="col-2 logo-icon">
         <span className="my_shop" onClick={()=>setShow(true)}>
-              <img src ="images/logo.png" alt="" width="200" height="50"/>
+              <img src ={imageLogo} alt="" width="200" height="50"/>
              </span>
          
             </div>
@@ -40,31 +54,31 @@ const Navbar = ({size, setShow}) => {
             </div></div>
             <div className='col-2'>
               <p className='text-black mb-0'>
-              <img src ="images/call.png" alt="" width="32" height="32"/> <a href='9817112439' className=''>9817112439</a>
+              <img src ={imageCall} alt="" width="32" height="32"/> <a href='9817112439' className=''>9817112439</a>
               </p>
             </div>
             <div className="col-2">
               <div className="header-upper-links d-flex align-items-center justify-content-between">
               <div>
                 <Link>
-                <img src='/images/bar-chart.png' alt=''></img>
+                <img src={barchart} alt=''></img>
                 </Link>
               </div>
               <div>
                 <Link to='/heart'>
-                <img src='/images/heart.png' alt=''></img>
+                <img src={heart} alt=''></img>
                 </Link>
               </div>
               <div>
                 <Link>
-                <img src='/images/user.png' alt=''></img>
+                <img src={imageUser} alt=''></img>
                 </Link>
               </div>
               <div>
                 
               <div className="cart" onClick={()=>setShow(false)}>
                 <span>
-                <img src='/images/shopping-cart.png' alt=''></img>
+                <img src={shoppingcart} alt=''></img>
                 </span>
                 <span>{size}</span>
             </div>
@@ -88,36 +102,36 @@ const Navbar = ({size, setShow}) => {
             <div className='menu-links'>
               <div className="d-flex align-items-center gap-15">
                 <NavLink to='/'>
-                <img src='/images/camera.png' width='30' height='30' className='navbar-icons-img' alt=''></img>
+                <img src={imageCamera} width='30' height='30' className='navbar-icons-img' alt=''></img>
                   Electronics
-                <img src='/images/down-arrow.png' alt=''>
+                <img src={downarrow} alt=''>
                 </img>
                 </NavLink>
                 <NavLink to='/'>
-                <img src='/images/sofa.png' width='30' height='30' className='navbar-icons-img' alt=''></img>
+                <img src={imageSofa} width='30' height='30' className='navbar-icons-img' alt=''></img>
 
                   Home Decor & Furnishing
-                <img src='/images/down-arrow.png' alt=''>
+                <img src={downarrow} alt=''>
 
                 </img>
                 </NavLink>
                 <NavLink to='/'>
-                <img src='/images/essentials.png' className='navbar-icons-img' width='30' height='30' alt=''></img>
+                <img src={essentials} className='navbar-icons-img' width='30' height='30' alt=''></img>
                   
                   Daily Essentials
-                <img src='/images/down-arrow.png' alt=''></img>
+                <img src={downarrow} alt=''></img>
                 </NavLink>
                 <NavLink to='/contact'>
-                <img src='/images/football.png' width='30' height='30' className='navbar-icons-img' alt=''></img>
+                <img src={imageFootball} width='30' height='30' className='navbar-icons-img' alt=''></img>
                   
                   Sports & Outdoor
-                <img src='/images/down-arrow.png' alt=''></img>
+                <img src= {downarrow} alt=''></img>
                 </NavLink>
                 <NavLink to='/contact'>
-                <img src='/images/dress.png' width='30' height='30' className='navbar-icons-img' alt=''></img>
+                <img src={imageDress} width='30' height='30' className='navbar-icons-img' alt=''></img>
                   
                   Women's Wear
-                <img src='/images/down-arrow.png' alt=''></img>
+                <img src={downarrow} alt=''></img>
                 </NavLink>
               </div>
             </div>
